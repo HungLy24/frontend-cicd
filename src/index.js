9fe8beadfd15c031a66ec1f9d0a9f9314d3a684e
Hung <hungly0503@gmail.com>
2025-12-15 08:01:31 +0700

    init frontend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const API = process.env.REACT_APP_API_URL;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [notes, setNotes] = React.useState([]);
+  const [text, setText] = React.useState("");
+
+  const load = async () => {
+    const res = await fetch(API + "/api/notes");
+    const data = await res.json();
+    setNotes(data);
+  };
+
+  const add = async () => {
+    await fetch(API + "/api/notes", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text })
+    });
+    setText("");
+    load();
+  };
+
+  React.useEffect(() => { load(); }, []);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>React + Node + Mongo</h1>
+      <input value={text} onChange={e => setText(e.target.value)} />
+      <button onClick={add}>Add</button>
+      <ul>
+        {notes.map(n => <li key={n._id}>{n.text}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
